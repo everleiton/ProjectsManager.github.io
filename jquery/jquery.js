20b767771a96b2040d85941a11a988b4337885b3
Ever Leiton <everleitonm@gmail.com>
2016-07-25 09:21:37 -0600

    cambios

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
   //Oculta el menu al cargar la página
-  $("#menu").hide();
+  $("#context-menu").hide();
   //mostramos el menú con click derecho
   $(document).bind("contextmenu", function(e){
-    $("#menu").css({'display':'block', 'left':e.pageX, 'top':e.pageY});
+    $("#context-menu").css({'display':'block', 'left':e.pageX, 'top':e.pageY});
     return false;
   });
   //cuando hagamos click, el menú desaparecerá
   $(document).click(function(e){
     if(e.button == 0){
-      $("#menu").css("display", "none");
+      $("#context-menu").css("display", "none");
     }
   });
   //si pulsamos escape, el menú desaparecerá
   $(document).keydown(function(e){
     if(e.keyCode == 27){
-      $("#menu").css("display", "none");
+      $("#context-menu").css("display", "none");
     }
   });
 
   //controlamos los botones del menú
-  $("#menu").click(function(e){
+  $("#context-menu").click(function(e){
     switch(e.target.id){
       case "crear_proyecto":
 
